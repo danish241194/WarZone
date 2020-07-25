1e9cac002ebe463e6549c21b79db3cacd4edc8ca
Danish Mukhtar <47193743+danish241194@users.noreply.github.com>
2020-07-26 02:18:02 +0530

    add full bound collide with adjusted position

--- rocket.js
+++ rocket.js
@@ -1,8 +1,7 @@
-import { rotate } from "/src/utils";
+import DirectionBox from "./direction";
 
 let STABLE_STATE = 1;
 let ANIMATION_STATE = 2;
-let INTERMEDIATE_STATE = 3;
 
 let ANIMATION_FRAME_SKIP = 5;
 export default class Rocket {
@@ -10,118 +9,76 @@ export default class Rocket {
     this.ctx = ctx;
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
-    this.width = 4;
-    this.height = 15;
-    this.angle = 0;
-    this.position = {
-      x1: this.gameWidth / 2 - this.width / 2,
-      y1: this.gameHeight - 10,
-      x2: this.gameWidth / 2 - this.width / 2,
-      y2: this.gameHeight - this.height - 10
+
+    this.act_pos = {
+      x: this.gameWidth / 2 - 7,
+      y: this.gameHeight - 55
     };
-    this.rotation_degree = 10;
+
     this.state = STABLE_STATE;
+    this.direction = new DirectionBox(this.ctx, gameWidth, gameHeight);
   }
 
   rotateLeft() {
-    let new_x = rotate(
-      this.position.x1,
-      this.position.y1,
-      this.position.x2,
-      this.position.y2,
-      this.rotation_degree,
-      true
-    );
-
-    if (
-      new_x.x < 0 ||
-      new_x.x > this.gameWidth ||
-      new_x.y < 0 ||
-      new_x.y > this.gameHeight
-    ) {
-      return;
-    }
-    this.position.x2 = new_x.x;
-    this.position.y2 = new_x.y;
+    this.direction.rotateLeft();
   }
 
   rotateRight() {
-    let new_x = rotate(
-      this.position.x1,
-      this.position.y1,
-      this.position.x2,
-      this.position.y2,
-      this.rotation_degree,
-      false
-    );
-    if (
-      new_x.x < 0 ||
-      new_x.x > this.gameWidth ||
-      new_x.y < 0 ||
-      new_x.y > this.gameHeight
-    ) {
-      return;
-    }
-    this.position.x2 = new_x.x;
-    this.position.y2 = new_x.y;
+    this.direction.rotateRight();
   }
 
   shoot(time) {
-    console.log("time = ", time);
     this.animation_target_time = time * 10;
-    let tx = this.position.x2 - this.position.x1;
-    let ty = this.position.y2 - this.position.y1;
-    let dist = Math.sqrt(tx * tx + ty * ty);
-
-    this.velX = (tx / dist) * ANIMATION_FRAME_SKIP;
-    this.velY = (ty / dist) * ANIMATION_FRAME_SKIP;
+    let velocity = this.direction.get_velX_velY();
+    this.velX = velocity.velx * ANIMATION_FRAME_SKIP;
+    this.velY = velocity.vely * ANIMATION_FRAME_SKIP;
     this.state = ANIMATION_STATE;
+    console.log("time = ", time);
   }
 
   draw() {
     if (this.state === ANIMATION_STATE) {
-      if (
-        this.position.x1 + this.velX >= 0 &&
-        this.position.x1 + this.velX + 10 <= this.gameWidth &&
-        this.position.y1 + this.velY >= 0 &&
-        this.position.y1 + this.velY + 10 <= this.gameHeight
-      ) {
-        this.position.x2 += this.velX;
-        this.position.y2 += this.velY;
-        this.position.x1 += this.velX;
-        this.position.y1 += this.velY;
+      let collide_result = this.direction.colide_position(
+        this.act_pos.x,
+        this.act_pos.y,
+        this.act_pos.x + this.velX,
+        this.act_pos.y + this.velY,
+        this.velX,
+        this.velY
+      );
 
-        if (this.position.x2 <= 0) {
-          this.position.x2 = this.position.x1 + 15;
-          this.position.y2 = this.position.y1;
-          this.state = STABLE_STATE;
-
-          //chnage needed
-        } else if (this.position.x2 + 15 >= this.gameWidth) {
-          console.log("yes");
-          this.position.x2 = this.position.x1 - 15;
-          this.position.y2 = this.position.y1;
-          this.ctx.fillRect(this.position.x2, this.position.y2, 50, 50);
-        }
-        this.animation_target_time -= 2;
-        if (this.animation_target_time <= 5) {
-          this.state = INTERMEDIATE_STATE;
-        }
+      if (collide_result.isColliding) {
+        this.act_pos.x = collide_result.x;
+        this.act_pos.y = collide_result.y;
+        this.state = STABLE_STATE;
+      } else {
+        this.act_pos.x += this.velX;
+        this.act_pos.y += this.velY;
       }
-    } else if (this.state === INTERMEDIATE_STATE) {
-      this.animation_target_time -= 1;
 
+      if (this.act_pos.x < 0) {
+        this.act_pos.x = 0;
+        this.state = STABLE_STATE;
+      } else if (this.act_pos.x + 10 > this.gameWidth) {
+        this.act_pos.x = this.gameWidth - 10;
+        this.state = STABLE_STATE;
+      }
+
+      if (this.act_pos.y + 10 > this.gameHeight) {
+        this.act_pos.y = this.gameHeight - 10;
+        this.state = STABLE_STATE;
+      } else if (this.act_pos.y < 0) {
+        this.act_pos.y = 0;
+        this.state = STABLE_STATE;
+      }
+
+      this.animation_target_time -= 2;
       if (this.animation_target_time <= 0) {
         this.state = STABLE_STATE;
       }
-    } else {
-      this.ctx.strokeStyle = "#ff00ff";
-      this.ctx.lineWidth = 4;
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.position.x1, this.position.y1);
-      this.ctx.lineTo(this.position.x2, this.position.y2);
-      this.ctx.stroke();
     }
-    this.ctx.fillRect(this.position.x1 - 5, this.position.y1 - 5, 10, 10);
+    this.direction.draw(this.state);
+
+    this.ctx.fillRect(this.act_pos.x, this.act_pos.y, 10, 10);
   }
 }
