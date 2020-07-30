82f431698f7468878d23eaea7823e7051007c6f4
Danish Mukhtar <47193743+danish241194@users.noreply.github.com>
2020-07-30 23:21:42 +0530

    2 player interface

--- player.js
+++ player.js
@@ -2,10 +2,47 @@ import DirectionBox from "./direction";
 import Rocket from "/src/rocket";
 
 export default class Player {
-  constructor(ctx, GAME_WIDTH, GAME_HEIGHT, rockets_positions) {
+  constructor(ctx, GAME_WIDTH, GAME_HEIGHT, rockets_positions, pid) {
     this.total_rockets = rockets_positions.length;
     this.current_rocket = 0;
-    this.directionbox = new DirectionBox(ctx, GAME_WIDTH, GAME_HEIGHT);
+    let direction_width = 4;
+    let direction_height = 15;
+    var direction_x_y, box_dimension;
+    if (pid === 1) {
+      direction_x_y = {
+        x1: GAME_WIDTH / 2 - direction_width / 2,
+        y1: GAME_HEIGHT - 20,
+        x2: GAME_WIDTH / 2 - direction_width / 2,
+        y2: GAME_HEIGHT - direction_height - 20
+      };
+      box_dimension = {
+        x: direction_x_y.x1 - 20,
+        y: GAME_HEIGHT - 40,
+        width: 40,
+        height: 40
+      };
+    } else {
+      direction_x_y = {
+        x1: GAME_WIDTH / 2 - direction_width / 2,
+        y1: 20,
+        x2: GAME_WIDTH / 2 - direction_width / 2,
+        y2: direction_height + 20
+      };
+      box_dimension = {
+        x: direction_x_y.x1 - 20,
+        y: 0,
+        width: 40,
+        height: 40
+      };
+    }
+    this.directionbox = new DirectionBox(
+      ctx,
+      GAME_WIDTH,
+      GAME_HEIGHT,
+      direction_width,
+      direction_x_y,
+      box_dimension
+    );
     this.rockets = [];
     for (var i = 0; i < this.total_rockets; i++) {
       let rocket = new Rocket(
@@ -20,9 +57,10 @@ export default class Player {
     }
     this.rockets[0].set_circle_color("red");
   }
-  set_rocket(i) {
+  set_rocket() {
     this.rockets[this.current_rocket].set_circle_color("green");
-    this.current_rocket = i;
+    this.current_rocket += 1;
+    this.current_rocket %= this.total_rockets;
     this.rockets[this.current_rocket].set_circle_color("red");
   }
   draw() {
