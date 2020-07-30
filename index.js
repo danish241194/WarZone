fd50d0ca2ffe6096d509068442aecc994521f713
Danish Mukhtar <47193743+danish241194@users.noreply.github.com>
2020-07-30 23:21:08 +0530

    2 player interface

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ let GAME_WIDTH = 800;
 let GAME_HEIGHT = 600;
 ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
-let player = new Player(ctx, GAME_WIDTH, GAME_HEIGHT, [
-  [GAME_WIDTH / 2 +10, GAME_HEIGHT - 55],
-  [GAME_WIDTH / 2 - 7,GAME_HEIGHT - 55],
-  [GAME_WIDTH / 2 - 24,GAME_HEIGHT - 55]
-]);
-let handler = new InputHandler(player);
+let player2 = new Player(
+  ctx,
+  GAME_WIDTH,
+  GAME_HEIGHT,
+  [
+    [GAME_WIDTH / 2 + 10, 55],
+    [GAME_WIDTH / 2 - 7, 55],
+    [GAME_WIDTH / 2 - 24, 55]
+  ],
+  2
+);
+
+let player1 = new Player(
+  ctx,
+  GAME_WIDTH,
+  GAME_HEIGHT,
+  [
+    [GAME_WIDTH / 2 + 10, GAME_HEIGHT - 55],
+    [GAME_WIDTH / 2 - 7, GAME_HEIGHT - 55],
+    [GAME_WIDTH / 2 - 24, GAME_HEIGHT - 55]
+  ],
+  1
+);
+
+let handler = new InputHandler(player2);
 
 function gameLoop(timestamp) {
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-  player.draw();
+  player2.draw();
+  player1.draw();
   requestAnimationFrame(gameLoop);
 }
 
